b5b0fee5ba9d00ded60625b48ba136a6ac9e35e5
Medet Idrish <idrismedet0@gmail.com>
2024-10-07 23:31:27 +0500

    Job MVC

---
+++ src/main/java/com/medet/firstjobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,23 @@
+package com.medet.firstjobapp.job.impl;
+
+import com.medet.firstjobapp.job.Job;
+import com.medet.firstjobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+    private List<Job> jobs = new ArrayList<>();
+
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobs.add(job);
+    }
+}
